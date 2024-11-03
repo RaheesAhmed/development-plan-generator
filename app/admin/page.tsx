ee62fdbeff4c89d7530974da16cf2a45ce1fa8db
Rahees Ahmed <raheesahmed256@gmail.com>
2024-11-03 16:42:56 +0500

    ssrc fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,5 @@
-import dynamic from "next/dynamic";
-
-// Create a dynamic component with ssr disabled
-const AdminDashboardContent = dynamic(
-  () => import("@/components/AdminDashboardContent"),
-  { ssr: false }
-);
+"use client";
+import AdminDashboardContent from "@/components/AdminDashboardContent";
 
 export default function AdminPage() {
   return <AdminDashboardContent />;
