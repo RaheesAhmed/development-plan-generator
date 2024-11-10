0c8ea3d1395b1729236670db325854703da2fe03
Rahees Ahmed <raheesahmed256@gmail.com>
2024-11-10 18:07:46 +0500

    FIXED: useSearchParams() should be wrapped in a suspense boundary

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import LoginForm from "@/components/auth/LoginForm";
 
-export default function Login() {
-  return <LoginForm />;
+export default function LoginPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
